2369aab13c1533bc664f9020a0ba82e428bb7161
yordankozhuharov1-hash <yordan.kozhuharov.1@gmail.com>
2026-08-19 16:57:59 +0300

    Create people.py

---
+++ people.py
@@ -0,0 +1,182 @@
+famous_people = [
+    {
+        "name": "Cristiano Ronaldo",
+        "followers": 650000000,
+        "country": "Portugal",
+        "what_they_do": "Football player"
+    },
+    {
+        "name": "Lionel Messi",
+        "followers": 510000000,
+        "country": "Argentina",
+        "what_they_do": "Football player"
+    },
+    {
+        "name": "Selena Gomez",
+        "followers": 430000000,
+        "country": "United States",
+        "what_they_do": "Singer and actress"
+    },
+    {
+        "name": "Kylie Jenner",
+        "followers": 400000000,
+        "country": "United States",
+        "what_they_do": "Media personality and entrepreneur"
+    },
+    {
+        "name": "Dwayne Johnson",
+        "followers": 395000000,
+        "country": "United States",
+        "what_they_do": "Actor and former wrestler"
+    },
+    {
+        "name": "Ariana Grande",
+        "followers": 380000000,
+        "country": "United States",
+        "what_they_do": "Singer and actress"
+    },
+    {
+        "name": "Kim Kardashian",
+        "followers": 365000000,
+        "country": "United States",
+        "what_they_do": "Media personality and entrepreneur"
+    },
+    {
+        "name": "Beyonce",
+        "followers": 320000000,
+        "country": "United States",
+        "what_they_do": "Singer and songwriter"
+    },
+    {
+        "name": "Khloe Kardashian",
+        "followers": 305000000,
+        "country": "United States",
+        "what_they_do": "Media personality"
+    },
+    {
+        "name": "Justin Bieber",
+        "followers": 295000000,
+        "country": "Canada",
+        "what_they_do": "Singer"
+    },
+    {
+        "name": "Kendall Jenner",
+        "followers": 290000000,
+        "country": "United States",
+        "what_they_do": "Model and media personality"
+    },
+    {
+        "name": "Taylor Swift",
+        "followers": 285000000,
+        "country": "United States",
+        "what_they_do": "Singer and songwriter"
+    },
+    {
+        "name": "Virat Kohli",
+        "followers": 275000000,
+        "country": "India",
+        "what_they_do": "Cricketer"
+    },
+    {
+        "name": "Jennifer Lopez",
+        "followers": 250000000,
+        "country": "United States",
+        "what_they_do": "Singer and actress"
+    },
+    {
+        "name": "Neymar Jr",
+        "followers": 230000000,
+        "country": "Brazil",
+        "what_they_do": "Football player"
+    },
+    {
+        "name": "Nicki Minaj",
+        "followers": 225000000,
+        "country": "Trinidad and Tobago",
+        "what_they_do": "Rapper and singer"
+    },
+    {
+        "name": "Miley Cyrus",
+        "followers": 215000000,
+        "country": "United States",
+        "what_they_do": "Singer and actress"
+    },
+    {
+        "name": "Katy Perry",
+        "followers": 205000000,
+        "country": "United States",
+        "what_they_do": "Singer"
+    },
+    {
+        "name": "Zendaya",
+        "followers": 195000000,
+        "country": "United States",
+        "what_they_do": "Actress and singer"
+    },
+    {
+        "name": "Shakira",
+        "followers": 190000000,
+        "country": "Colombia",
+        "what_they_do": "Singer and songwriter"
+    },
+    {
+        "name": "LeBron James",
+        "followers": 180000000,
+        "country": "United States",
+        "what_they_do": "Basketball player"
+    },
+    {
+        "name": "Billie Eilish",
+        "followers": 175000000,
+        "country": "United States",
+        "what_they_do": "Singer and songwriter"
+    },
+    {
+        "name": "Drake",
+        "followers": 170000000,
+        "country": "Canada",
+        "what_they_do": "Rapper and singer"
+    },
+    {
+        "name": "Rihanna",
+        "followers": 165000000,
+        "country": "Barbados",
+        "what_they_do": "Singer and entrepreneur"
+    },
+    {
+        "name": "Tom Holland",
+        "followers": 160000000,
+        "country": "United Kingdom",
+        "what_they_do": "Actor"
+    },
+    {
+        "name": "Emma Watson",
+        "followers": 155000000,
+        "country": "United Kingdom",
+        "what_they_do": "Actress"
+    },
+    {
+        "name": "Chris Hemsworth",
+        "followers": 150000000,
+        "country": "Australia",
+        "what_they_do": "Actor"
+    },
+    {
+        "name": "MrBeast",
+        "followers": 145000000,
+        "country": "United States",
+        "what_they_do": "YouTuber and philanthropist"
+    },
+    {
+        "name": "Robert Downey Jr.",
+        "followers": 140000000,
+        "country": "United States",
+        "what_they_do": "Actor"
+    },
+    {
+        "name": "Scarlett Johansson",
+        "followers": 135000000,
+        "country": "United States",
+        "what_they_do": "Actress"
+    }
+]
